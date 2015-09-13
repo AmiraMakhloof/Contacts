00520b9b58a8d120ca0f6b39d735e0e23418c454
amiramakhoof <amiramakhloof@hotmail.com>
2015-09-14 00:24:43 +0300

    first push

---
+++ src/com/amiracontact/ContactTest.java
@@ -0,0 +1,23 @@
+package com.amiracontact;
+
+import android.app.ListActivity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.Contacts.People;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.widget.ListAdapter;
+
+public class ContactTest extends ListActivity {
+	// ListAdapter mAdapter;
+	// public static final int ACTIVITY_EDIT =1;
+	// public static final int ACTIVITY_CREATE =0;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		
+		
+
+	}
+}
